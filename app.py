258960a41d50ebe55d14449ab744c3bca35fafaa
TARUNPREET <tarunsuri2697@gmail.com>
2018-10-14 12:12:08 +0530

    updated

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.feature_extraction.text import TfidfTransformer 
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import preprocessing
+import pandas as pd
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+tfidf = joblib.load('tfidf_joblib')
+
+# parser = reqparse.RequestParser()
+# parser.add_argument('query')
+
+class PredictSentiment(Resource):
+    def get(self,query):
+        if query=='cong':
+            test=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR5cg9wx36ZoCO3BK4ePB_OCA3kFphDxmRaxoGHqggsdsn3p2ziOO0Ss3CvwuSb3IAbL3Btj5Vc_SI_/pub?output=csv')
+            #test=pd.read_csv('test1.csv', encoding='Windows-1252')
+            X_test=test['SentimentText'].values
+            X_test = tfidf.transform(X_test)
+            X_test = preprocessing.normalize(X_test)
+            X_test = X_test.todense()
+        elif query=='bjp':
+            test=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSKmNP2Au_O1LTxo1rSd4NaWTSFCNDHx6a25hQzohcbZ-BH9yBjbJW92wMUm4S_mrpLV0gw2nuHmwkS/pub?output=csv')
+            #test=pd.read_csv('test1.csv', encoding='Windows-1252')
+            X_test=test['SentimentText'].values
+            X_test = tfidf.transform(X_test)
+            X_test = preprocessing.normalize(X_test)
+            X_test = X_test.todense()
+        jb=joblib.load('model_joblib')
+        pred=jb.predict(X_test)
+        positive=0
+        negative=0
+        for i in pred:
+            if i==0:    
+                negative=negative+1
+            else:
+                positive=positive+1
+        if positive > negative:
+            pred='positive'
+        else:
+            pred='negative'
+        positive_perc=(float(positive)/(positive+negative))*100
+        negative_perc=(float(negative)/(positive+negative))*100
+        output={'prediction':pred,'positive_no':positive,'negative_no':negative,'positive_per':positive_perc,'negative_per':negative_perc,'party':query}
+        return output
+
+api.add_resource(PredictSentiment, '/<string:query>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- project1.py
+++ project1.py
@@ -1,12 +1,14 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 from sklearn.externals import joblib
-from sklearn.cross_validation import train_test_split
+# from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-from sklearn.cross_validation import cross_val_score
+#from sklearn.cross_validation import cross_val_score
 from collections import Counter
 from sklearn.metrics import accuracy_score
-from sklearn import cross_validation
+#from sklearn import cross_validation
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.feature_extraction.text import TfidfTransformer 
 from sklearn.feature_extraction.text import TfidfVectorizer 
@@ -21,27 +23,38 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
 
+app = Flask(__name__)
+api = Api(app)
+
 
 tfidf = joblib.load('tfidf_joblib')
 test=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR5cg9wx36ZoCO3BK4ePB_OCA3kFphDxmRaxoGHqggsdsn3p2ziOO0Ss3CvwuSb3IAbL3Btj5Vc_SI_/pub?output=csv')
-X_test=test['SentimentText'].values;
+#test=pd.read_csv('test1.csv', encoding='Windows-1252')
+X_test=test['SentimentText'].values
 X_test = tfidf.transform(X_test)
 X_test = preprocessing.normalize(X_test)
 X_test = X_test.todense()
 jb=joblib.load('model_joblib')
 
-neighbors = [0.0001,0.001,0.01,0.1, 1,10,100,1000]
-for hyperparameter in neighbors:
-	pred=jb.predict(X_test)
+parser = reqparse.RequestParser()
 
-for i in pred:
-    print(i)
-    print(" ")
+def posneg():
+    if Positive > Negative:
+        pred='positive'
+    else:
+        pred='negative'
+class PredictSentiment(Resource):
+	def get(self):
+		pred=jb.predict(X_test)
+		Negative = (pred == 0).sum()
+		Positive = (pred == 1).sum()
+        # create JSON object
+        posneg()
+        output = {'prediction': pred, 'positive_rate': Positive,'Negative_rate': Negative }
+        return output
 
-print (pred)
-Negative = (pred == 0).sum()
-Positive = (pred == 1).sum()
-print("Number of Positive Reviews:")
-print(Positive)
-print("Number of Negative Reviews:")
-print(Negative)
+api.add_resource(PredictSentiment, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
